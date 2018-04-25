b48248bc4486967d14a46b5fd8321f72635bd98a
siqingbushou <siqingbushou@163.com>
2018-04-25 20:18:53 +0800

    Update reasoning.m

--- reasoning.m
+++ reasoning.m
@@ -1,4 +1,4 @@
-ANS = [1;2;3;4];   %µÚÒ»Ìâ
+ANS = [1;2;3;4];   %ç¬¬ä¸€é¢˜
 a2xuan = [3 4 1 2];
 a3xuan = [3 6 2 4];
 a4xuan = [1 5;2 7;1 9;6 10];
@@ -9,25 +9,25 @@ a8xuan = [7 5 2 10];
 a9xuan = [6 10 2 9];
 a10xuan = [3 2 4 1];
 a = ones(1,10);
-todelete = [];
+
 for  n = 2:10 
     ANS = [ANS,ones(length(ANS),1);...
            ANS,ones(length(ANS),1)*2;...
            ANS,ones(length(ANS),1)*3;...
-           ANS,ones(length(ANS),1)*4]; %ÁĞ¾ÙÇ°nÌâËùÓĞ´ğ°¸
+           ANS,ones(length(ANS),1)*4]; %åˆ—ä¸¾æ‰€æœ‰ç­”æ¡ˆ
 end
 for m = 1:length(ANS)   
-    a = ANS(m,:);      %ÌáÈ¡Ç°nÌâËùÓĞ´ğ°¸µÚmÖÖ
+    a = ANS(m,:);      %æå–æ‰€æœ‰ç­”æ¡ˆç¬¬mç§
     truescore = (a(5) == a2xuan(a(2)))&...     %#2
                 (sum(a(a3xuan(:))==a(a3xuan(a(3))))==1)&...        %#3
                 (a(a4xuan(a(4),1))==a(a4xuan(a(4),2)))&...           %#4 
                 (a(a5xuan(a(5)))==a(5))&...            %#5 
-                (a(a6xuan(a(6),1))==a(8)&a(8)==a(a6xuan(a(6),2)))&... 
-                ((length(find(a==a7xuan(a(7)))))==(min(hist(a,1:4))))&...%#6 
+                (a(a6xuan(a(6),1))==a(8)&a(8)==a(a6xuan(a(6),2)))&...  %#6
+                ((length(find(a==a7xuan(a(7)))))==(min(hist(a,1:4))))&... 
                 (abs(a(a8xuan(a(8)))-a(1))~=1)&...
                 (((a(1)==a(6))+(a(a9xuan(a(9)))==a(5)))==1)&...
                 ((max(hist(a,1:4)))-(min(hist(a,1:4))))==a10xuan(a(10));               
-    if truescore == 1       %ÑéÖ¤µÚmÖĞÊÇ·ñ·ûºÏ
+    if truescore == 1       %éªŒè¯ç¬¬mç§æ˜¯å¦ç¬¦åˆ
         disp(['the answer is ' char(a+64)]) 
         return
     end
